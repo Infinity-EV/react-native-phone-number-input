14e2db3fd83662b90ba1030e5d25ef2c80f3419a
Aly Ezz <88483473+alyezzz@users.noreply.github.com>
2026-02-07 23:56:36 +0200

    Add set country

--- src/index.tsx
+++ src/index.tsx
@@ -1,345 +1,236 @@
-import { PhoneNumberUtil } from "google-libphonenumber";
-import React from "react";
+import { PhoneNumberUtil } from "google-libphonenumber"
+import React from "react"
 import {
-    Image,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-    type StyleProp,
-    type TextInputProps,
-    type TextStyle,
-    type ViewStyle
-} from "react-native";
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  type StyleProp,
+  type TextInputProps,
+  type TextStyle,
+  type ViewStyle,
+} from "react-native"
 
 import CountryPicker, {
-    CountryModalProvider,
-    DARK_THEME,
-    DEFAULT_THEME,
-    Flag,
-    getCallingCode,
-    loadDataAsync,
-    type CallingCode,
-    type Country,
-    type CountryCode,
-    type CountryFilterProps,
-    type CountryPickerModalProps
-} from "./countryPickerModal";
-import { applyMask, getMaskForCountry, removeMask } from "./maskUtils";
-import styles from "./styles";
+  CountryModalProvider,
+  DARK_THEME,
+  DEFAULT_THEME,
+  Flag,
+  getCallingCode,
+  loadDataAsync,
+  type CallingCode,
+  type Country,
+  type CountryCode,
+  type CountryFilterProps,
+  type CountryPickerModalProps,
+} from "./countryPickerModal"
+import { applyMask, getMaskForCountry, removeMask } from "./maskUtils"
+import styles from "./styles"
 
 const dropDown =
-    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAi0lEQVRYR+3WuQ6AIBRE0eHL1T83FBqU5S1szdiY2NyTKcCAzU/Y3AcBXIALcIF0gRPAsehgugDEXnYQrUC88RIgfpuJ+MRrgFmILN4CjEYU4xJgFKIa1wB6Ec24FuBFiHELwIpQxa0ALUId9wAkhCnuBdQQ5ngP4I9wxXsBDyJ9m+8y/g9wAS7ABW4giBshQZji3AAAAABJRU5ErkJggg==";
-const phoneUtil = PhoneNumberUtil.getInstance();
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAi0lEQVRYR+3WuQ6AIBRE0eHL1T83FBqU5S1szdiY2NyTKcCAzU/Y3AcBXIALcIF0gRPAsehgugDEXnYQrUC88RIgfpuJ+MRrgFmILN4CjEYU4xJgFKIa1wB6Ec24FuBFiHELwIpQxa0ALUId9wAkhCnuBdQQ5ngP4I9wxXsBDyJ9m+8y/g9wAS7ABW4giBshQZji3AAAAABJRU5ErkJggg=="
 
-export type PhoneInputProps = {
-    withDarkTheme?: boolean;
-    withShadow?: boolean;
-    withMask?: boolean;
-    autoFocus?: boolean;
-    defaultCode?: CountryCode;
-    defaultCallingCode?: string;
-    value?: string;
-    defaultValue?: string;
-    disabled?: boolean;
-    disableArrowIcon?: boolean;
-    placeholder?: string;
-    onChangeCountry?: (country: Country) => void;
-    onChangeText?: (text: string) => void;
-    onChangeFormattedText?: (text: string) => void;
-    onBlur?: () => void;
-    onFocus?: () => void;
-    renderDropdownImage?: React.ReactNode;
-    containerStyle?: StyleProp<ViewStyle>;
-    textContainerStyle?: StyleProp<ViewStyle>;
-    textInputProps?: TextInputProps;
-    textInputStyle?: StyleProp<TextStyle>;
-    codeTextStyle?: StyleProp<TextStyle>;
-    flagButtonStyle?: StyleProp<ViewStyle>;
-    countryPickerButtonStyle?: StyleProp<ViewStyle>;
-    layout?: "first" | "second";
-    filterProps?: CountryFilterProps;
-    countryPickerProps?: CountryPickerModalProps;
-    flagSize?: number;
-    showCountryCode?: boolean;
-};
+const phoneUtil = PhoneNumberUtil.getInstance()
 
 export type PhoneInputRefType = {
-    getCountryCode: () => CountryCode;
-    getCallingCode: () => CallingCode | undefined;
-    isValidNumber: (number: string) => boolean;
+  getCountryCode: () => CountryCode
+  getCallingCode: () => CallingCode | undefined
+  isValidNumber: (number: string) => boolean
+  getNumberAfterPossiblyEliminatingZero: () => {
+    number: string | undefined
+    formattedNumber: string | undefined
+  }
+  setCountryCode: (cca2: CountryCode) => Promise<void>
+  setCallingCode: (callingCode: string) => Promise<void>
+}
+
+const PhoneInput = React.forwardRef<PhoneInputRefType, any>((props, ref) => {
+  const getCountryCodeByCallingCode = React.useCallback(
+    async (callingCode: string) => {
+      const countries = await loadDataAsync()
+      if (!countries) return "US"
+
+      const entry = Object.entries(countries).find(
+        ([_, c]) => c.callingCode[0] === callingCode
+      )
+
+      return entry ? (entry[0] as CountryCode) : "US"
+    },
+    []
+  )
+
+  const [countryCode, setCountryCode] = React.useState<CountryCode>(
+    props.defaultCode || "US"
+  )
+  const [code, setCode] = React.useState<string | undefined>(undefined)
+  const [number, setNumber] = React.useState<string>("")
+  const [displayValue, setDisplayValue] = React.useState<string>("")
+  const [modalVisible, setModalVisible] = React.useState(false)
+  const [disabled, setDisabled] = React.useState(!!props.disabled)
+
+  React.useEffect(() => {
+    if (props.defaultCallingCode) {
+      getCountryCodeByCallingCode(props.defaultCallingCode).then((cca2) => {
+        setCountryCode(cca2)
+        setCode(props.defaultCallingCode)
+      })
+    }
+  }, [props.defaultCallingCode, getCountryCodeByCallingCode])
+
+  React.useEffect(() => {
+    if (props.defaultCode) {
+      getCallingCode(props.defaultCode).then(setCode)
+    }
+  }, [props.defaultCode])
+
+  React.useEffect(() => {
+    setDisabled(!!props.disabled)
+  }, [props.disabled])
+
+  const applyCountry = React.useCallback(
+    (country: Country) => {
+      setCountryCode(country.cca2)
+      setCode(country.callingCode[0])
+
+      if (props.withMask) {
+        const mask = getMaskForCountry(country.cca2)
+        setDisplayValue(number ? applyMask(number, mask) : "")
+      }
+
+      props.onChangeCountry?.(country)
+    },
+    [number, props]
+  )
+
+  const onSelect = React.useCallback(
+    (country: Country) => {
+      applyCountry(country)
+      setModalVisible(false)
+    },
+    [applyCountry]
+  )
+
+  const onChangeText = React.useCallback(
+    (text: string) => {
+      const raw = props.withMask ? removeMask(text) : text
+
+      if (!raw) {
+        setNumber("")
+        setDisplayValue("")
+        props.onChangeText?.("")
+        props.onChangeFormattedText?.("")
+        return
+      }
+
+      setNumber(raw)
+
+      if (props.withMask) {
+        const mask = getMaskForCountry(countryCode)
+        setDisplayValue(applyMask(raw, mask))
+      }
+
+      props.onChangeText?.(raw)
+
+      if (props.onChangeFormattedText) {
+        props.onChangeFormattedText(code ? `+${code}${raw}` : raw)
+      }
+    },
+    [code, countryCode, props]
+  )
+
+  React.useImperativeHandle(ref, () => ({
+    getCountryCode: () => countryCode,
+    getCallingCode: () => code,
+
+    setCountryCode: async (cca2: CountryCode) => {
+      const callingCode = await getCallingCode(cca2)
+      if (!callingCode) return
+
+      applyCountry({
+        cca2,
+        callingCode: [callingCode],
+      } as Country)
+    },
+
+    setCallingCode: async (callingCode: string) => {
+      const cca2 = await getCountryCodeByCallingCode(callingCode)
+
+      applyCountry({
+        cca2,
+        callingCode: [callingCode],
+      } as Country)
+    },
+
+    isValidNumber: (value: string) => {
+      try {
+        const parsed = phoneUtil.parse(value, countryCode)
+        return phoneUtil.isValidNumber(parsed)
+      } catch {
+        return false
+      }
+    },
+
     getNumberAfterPossiblyEliminatingZero: () => {
-        number: string | undefined;
-        formattedNumber: string | undefined;
-    };
-};
+      const clean = number.startsWith("0") ? number.slice(1) : number
+      return {
+        number: clean,
+        formattedNumber: code ? `+${code}${clean}` : clean,
+      }
+    },
+  }))
 
-const PhoneInput = React.forwardRef<PhoneInputRefType, PhoneInputProps>((props, ref) => {
-    const getCountryCodeByCallingCode = React.useCallback(async (callingCode: string) => {
-        const countries = await loadDataAsync();
-        if (!countries) return "US";
+  const {
+    withShadow,
+    withDarkTheme,
+    containerStyle,
+    textContainerStyle,
+    textInputStyle,
+    textInputProps,
+    placeholder,
+    layout = "first",
+    showCountryCode = true,
+    countryPickerProps = {
+      theme: withDarkTheme ? DARK_THEME : DEFAULT_THEME,
+    },
+  } = props
 
-        const countryEntry = Object.entries(countries).find(([_, country]) => country.callingCode[0] === callingCode);
+  return (
+    <CountryModalProvider>
+      <View style={[styles.container, withShadow && styles.shadow, containerStyle]}>
+        <TouchableOpacity
+          disabled={disabled}
+          style={styles.flagButtonView}
+          onPress={() => setModalVisible(true)}
+        >
+          <CountryPicker
+            visible={modalVisible}
+            onClose={() => setModalVisible(false)}
+            countryCode={countryCode}
+            withCallingCode
+            withFlag
+            onSelect={onSelect}
+            {...countryPickerProps}
+          />
+        </TouchableOpacity>
 
-        return countryEntry ? (countryEntry[0] as CountryCode) : "US";
-    }, []);
+        <View style={[styles.textContainer, textContainerStyle]}>
+          {showCountryCode && code && (
+            <Text style={styles.codeText}>{`+${code}`}</Text>
+          )}
+          <TextInput
+            value={props.withMask ? displayValue : number}
+            onChangeText={onChangeText}
+            placeholder={placeholder}
+            keyboardType="phone-pad"
+            editable={!disabled}
+            style={[styles.numberText, textInputStyle]}
+            {...textInputProps}
+          />
+        </View>
+      </View>
+    </CountryModalProvider>
+  )
+})
 
-    const [code, setCode] = React.useState<string | undefined>(
-        props.defaultCallingCode || (props.defaultCode ? undefined : "91")
-    );
-    const [number, setNumber] = React.useState<string | undefined>(undefined);
-    const [displayValue, setDisplayValue] = React.useState<string>("");
-    const [modalVisible, setModalVisible] = React.useState<boolean>(false);
-    const [countryCode, setCountryCode] = React.useState<CountryCode>(props.defaultCode || "IN");
-    const [disabled, setDisabled] = React.useState<boolean>(props.disabled || false);
-
-    React.useEffect(() => {
-        const setupDefaultCallingCode = async () => {
-            if (props.defaultCallingCode) {
-                // eslint-disable-next-line @typescript-eslint/no-shadow
-                const countryCode = await getCountryCodeByCallingCode(props.defaultCallingCode);
-                setCountryCode(countryCode);
-                setCode(props.defaultCallingCode);
-            }
-        };
-
-        setupDefaultCallingCode();
-    }, [props.defaultCallingCode, getCountryCodeByCallingCode]);
-
-    React.useEffect(() => {
-        const loadDefaultCode = async () => {
-            if (props.defaultCode) {
-                const callingCode = await getCallingCode(props.defaultCode);
-                setCode(callingCode);
-            }
-        };
-        loadDefaultCode();
-    }, [props.defaultCode]);
-
-    React.useEffect(() => {
-        if (props.disabled !== disabled) {
-            setDisabled(props.disabled || false);
-        }
-    }, [disabled, props.disabled]);
-
-    const onSelect = React.useCallback(
-        (country: Country) => {
-            setCountryCode(country.cca2);
-            setCode(country.callingCode[0]);
-
-            // Re-apply mask with new country pattern if masking is enabled
-            if (props.withMask && number) {
-                const newMask = getMaskForCountry(country.cca2);
-                const masked = applyMask(number, newMask);
-                setDisplayValue(masked);
-            }
-
-            if (props.onChangeFormattedText) {
-                if (country.callingCode[0]) {
-                    props.onChangeFormattedText(`+${country.callingCode[0]}${number}`);
-                } else {
-                    props.onChangeFormattedText(number || "");
-                }
-            }
-
-            if (props.onChangeCountry) {
-                props.onChangeCountry(country);
-            }
-        },
-        [number, props]
-    );
-
-    const onChangeText = React.useCallback(
-        (text: string) => {
-            const { withMask = false } = props;
-
-            if (withMask) {
-                // Get the mask pattern for current country
-                const mask = getMaskForCountry(countryCode);
-
-                // Remove mask from input to get raw digits
-                const rawDigits = removeMask(text);
-
-                // Apply mask to get display value
-                const masked = applyMask(rawDigits, mask);
-
-                // Update display value
-                setDisplayValue(masked);
-
-                // Store raw digits internally
-                setNumber(rawDigits);
-
-                // Pass raw digits to onChangeText callback
-                if (props.onChangeText) {
-                    props.onChangeText(rawDigits);
-                }
-
-                // Pass masked value with country code to onChangeFormattedText
-                if (props.onChangeFormattedText) {
-                    if (code) {
-                        props.onChangeFormattedText(rawDigits.length > 0 ? `+${code}${rawDigits}` : rawDigits);
-                    } else {
-                        props.onChangeFormattedText(rawDigits);
-                    }
-                }
-            } else {
-                // Original behavior when masking is disabled
-                setNumber(text);
-                setDisplayValue(text);
-
-                if (props.onChangeText) {
-                    props.onChangeText(text);
-                }
-                if (props.onChangeFormattedText) {
-                    if (code) {
-                        props.onChangeFormattedText(text.length > 0 ? `+${code}${text}` : text);
-                    } else {
-                        props.onChangeFormattedText(text);
-                    }
-                }
-            }
-        },
-        [code, countryCode, props]
-    );
-
-    const renderDefaultDropdownImage = React.useMemo(() => {
-        return <Image source={{ uri: dropDown }} resizeMode="contain" style={styles.dropDownImage} />;
-    }, []);
-
-    const renderFlagButton = React.useCallback(() => {
-        const { layout = "first", flagSize } = props;
-        if (layout === "first") {
-            return <Flag countryCode={countryCode} flagSize={flagSize || DEFAULT_THEME.flagSize} />;
-        }
-        return null;
-    }, [countryCode, props]);
-
-    React.useImperativeHandle(ref, () => ({
-        getCountryCode: () => countryCode,
-        getCallingCode: () => code,
-        isValidNumber: (phoneNumber: string) => {
-            if (!phoneNumber || !countryCode) {
-                return false;
-            }
-            try {
-                let cleanNumber = phoneNumber.replace(/[^\d+]/g, "");
-                if (cleanNumber.startsWith("0")) {
-                    cleanNumber = cleanNumber.substring(1);
-                }
-                if (!cleanNumber) {
-                    return false;
-                }
-                const parsedNumber = phoneUtil.parse(cleanNumber, countryCode);
-                return phoneUtil.isValidNumber(parsedNumber);
-            } catch (err) {
-                try {
-                    if (code) {
-                        let cleanNumber = phoneNumber.replace(/[^\d+]/g, "");
-                        if (cleanNumber.startsWith("0")) {
-                            cleanNumber = cleanNumber.substring(1);
-                        }
-                        const parsedNumber = phoneUtil.parse(cleanNumber, code);
-                        return phoneUtil.isValidNumber(parsedNumber);
-                    }
-                } catch (fallbackErr) {
-                    return false;
-                }
-                return false;
-            }
-        },
-        getNumberAfterPossiblyEliminatingZero: () => {
-            let currentNumber = number;
-            if (currentNumber && currentNumber.length > 0 && currentNumber.startsWith("0")) {
-                currentNumber = currentNumber.slice(1);
-            }
-            return {
-                number: currentNumber,
-                formattedNumber: code ? `+${code}${currentNumber}` : currentNumber
-            };
-        }
-    }));
-
-    const {
-        withShadow,
-        withDarkTheme,
-        withMask = false,
-        codeTextStyle,
-        textInputProps,
-        textInputStyle,
-        autoFocus,
-        placeholder,
-        disableArrowIcon,
-        flagButtonStyle,
-        containerStyle,
-        textContainerStyle,
-        renderDropdownImage = renderDefaultDropdownImage,
-        countryPickerProps = {
-            theme: withDarkTheme ? DARK_THEME : DEFAULT_THEME
-        },
-        filterProps = {},
-        countryPickerButtonStyle,
-        layout = "first",
-        onBlur,
-        onFocus,
-        showCountryCode = true
-    } = props;
-
-    return (
-        <CountryModalProvider>
-            <View style={[styles.container, withShadow && styles.shadow, containerStyle]}>
-                <TouchableOpacity
-                    style={[
-                        styles.flagButtonView,
-                        layout === "second" && styles.flagButtonExtraWidth,
-                        flagButtonStyle,
-                        countryPickerButtonStyle
-                    ]}
-                    disabled={disabled}
-                    onPress={() => setModalVisible(true)}>
-                    <CountryPicker
-                        onSelect={onSelect}
-                        withEmoji
-                        withFilter
-                        withFlag
-                        filterProps={filterProps}
-                        countryCode={countryCode}
-                        withCallingCode
-                        visible={modalVisible}
-                        renderFlagButton={renderFlagButton}
-                        onClose={() => setModalVisible(false)}
-                        {...countryPickerProps}
-                    />
-                    {showCountryCode && code && layout === "second" && (
-                        <Text style={[styles.codeText, codeTextStyle]}>{`+${code}`}</Text>
-                    )}
-                    {!disableArrowIcon && <React.Fragment>{renderDropdownImage}</React.Fragment>}
-                </TouchableOpacity>
-                <View style={[styles.textContainer, textContainerStyle]}>
-                    {showCountryCode && code && layout === "first" && (
-                        <Text style={[styles.codeText, codeTextStyle]}>{`+${code}`}</Text>
-                    )}
-                    <TextInput
-                        style={[styles.numberText, textInputStyle]}
-                        placeholder={placeholder}
-                        onChangeText={onChangeText}
-                        value={
-                            withMask
-                                ? displayValue || props.value || props.defaultValue || ""
-                                : number || props.value || props.defaultValue || ""
-                        }
-                        editable={!disabled}
-                        selectionColor="black"
-                        keyboardAppearance={withDarkTheme ? "dark" : "default"}
-                        keyboardType="number-pad"
-                        autoFocus={autoFocus}
-                        onBlur={onBlur}
-                        onFocus={onFocus}
-                        {...textInputProps}
-                    />
-                </View>
-            </View>
-        </CountryModalProvider>
-    );
-});
-
-export default PhoneInput;
+export default PhoneInput
